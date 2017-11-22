46858dd762509030cec6d297184e88b06b96038d
Chetan Kapoor <chetankapoor321@gmail.com>
2017-11-22 17:24:00 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spy
+
+A single line script to take screenshot every 10 seconds with timestamp.
---
+++ spy.sh
@@ -0,0 +1,6 @@
+NOWT=$(date +"%T")
+while :
+do
+    screencapture ~/test/$(date +%y%m%d%H%M%S).png 
+    sleep 10
+done
