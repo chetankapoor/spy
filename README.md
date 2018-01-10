6c496d51b04f5a2f6d79a9c8476783498d0d063d
Chetan Kapoor <chetankapoor321@gmail.com>
2018-01-10 12:29:20 +0100

    spy updated

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# spy
+ # spy
 
 A single line script to take screenshot on mac every 10 seconds with timestamp.
+
+Usage: 
+=====
+
+Download the Script:
+`wget https://raw.githubusercontent.com/chetankapoor/spy/master/spy.sh -O spy`
+
+Then run the script with following format:
+`sh spy 10 /Users/user/Pictures`
+
+
+
--- spy.sh
+++ spy.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
+
+set -o errexit
+
+time_interval=$1
+directory=$2
+
+if [ -z "$time_interval" -o -z "$directory" ]; then
+  echo "Usage:"
+  echo "spy time_interval directory"
+  printf '%s\n' "$@"
+
+  echo "---------------------------------------"
+  echo "Available options:"
+  printf '%s\n' "$@"
+  echo "time_interval - Time Interval to take the screenshot"
+  echo "directory - Where to save the screenshot"
+  exit 1
+fi
+
 while :
 do
-    screencapture ~/test/spy-$(date +%y%m%d%H%M%S).png
-    sleep 10
+    screencapture -x "$directory"/"spy-""$(date +%y%m%d%H%M%S).png"
+    sleep "$time_interval"
 done
